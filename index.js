57fdc1572dee18feba63a85013ada3e58f08e658
Taylorbelk1 <belktaylor12@gmail.com>
2019-03-06 21:56:10 -0600

    fixed error in the console log

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./api/server.js');
 PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log(`\n*** Server Running on http://localhost:${PORT}***\n`);
+  console.log(`\n*** Server Running on PORT ${PORT}`);
 });
